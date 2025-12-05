5f407943970c8932647271a64d2bb292f1fabbf5
gerocs96 <gerocs96@gmail.com>
2025-12-05 17:30:12 +0100

    added middlewares

---
+++ EventManager.Api/EventManager.Api/Middlewares/LoggingMiddleware.cs
@@ -0,0 +1,36 @@
+﻿using System.Diagnostics;
+
+namespace EventManager.Api.Middlewares
+{
+    /// <summary>
+    /// Http request and response logging middleware
+    /// </summary>
+    public class LoggingMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<LoggingMiddleware> _logger;
+
+        public LoggingMiddleware(RequestDelegate next, ILogger<LoggingMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            _logger.LogInformation("HTTP {Method} {Path} started",
+                context.Request.Method,
+                context.Request.Path);
+
+            var watch = Stopwatch.StartNew();
+            await _next(context);
+            watch.Stop();
+
+            _logger.LogInformation("HTTP {Method} {Path} finished in {ElapsedMilliseconds}ms with status {StatusCode}",
+                context.Request.Method,
+                context.Request.Path,
+                watch.ElapsedMilliseconds,
+                context.Response.StatusCode);
+        }
+    }
+}
--- EventManager.Api/EventManager.Api/Program.cs
+++ EventManager.Api/EventManager.Api/Program.cs
@@ -1,25 +1,31 @@
 using EventManager.Api.Data;
+using EventManager.Api.Middlewares;
 using EventManager.Api.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
-
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// Services
 builder.Services.AddScoped<IEventService, EventService>();
+
+// DbContext
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+// Application start
 var app = builder.Build();
 var logger = app.Services.GetRequiredService<ILogger<Program>>();
 logger.LogInformation("Application started.");
+
+// Middlewares
+app.UseMiddleware<ExceptionHandlingMiddleware>();
+app.UseMiddleware<LoggingMiddleware>();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
